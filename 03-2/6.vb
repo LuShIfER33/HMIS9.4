a8baec83be1897e2d507b5e09a35d31626062f3b
LuShIfER33 <sulavsapkota33@gmail.com>
2024-07-31 13:58:01 +0545

    65 completed

---
+++ 03-2/6.vb
@@ -0,0 +1,13 @@
+﻿Public Class _6
+    Private Sub _6_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub GroupBox1_Enter(sender As Object, e As EventArgs) Handles GroupBox1.Enter
+
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+End Class
--- 03-2/My Project/Application.Designer.vb
+++ 03-2/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global._03_2._5
+            Me.MainForm = Global._03_2._6
         End Sub
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
